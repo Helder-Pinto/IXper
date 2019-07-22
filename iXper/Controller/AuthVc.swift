93ce068736e201e7d4b01c7473c7d82cf6084e71
Helder Pinto <helder.manuel.afonso.pinto@sap.com>
2019-07-22 09:35:10 +0200

    design login VC

---
+++ iXper/Controller/AuthVc.swift
@@ -0,0 +1,28 @@
+//
+//  AuthVc.swift
+//  iXper
+//
+//  Created by Afonso Pinto, Helder Manuel on 22/07/2019.
+//  Copyright © 2019 Helder Pinto. All rights reserved.
+//
+
+import UIKit
+
+class AuthVc: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+    @IBAction func loginBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func createAccBtnPressed(_ sender: Any) {
+    }
+}
